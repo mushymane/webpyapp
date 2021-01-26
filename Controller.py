c14fc2b8f151e305b0326e2f9f95b7f00598f0a7
Luigi Lantin <mushymanee@gmail.com>
2021-01-26 06:50:16 -0800

    changes

---
+++ Controller.py
@@ -0,0 +1,17 @@
+import web
+
+urls = (
+    '/', 'home'
+)
+
+app = web.application(urls, globals())
+
+
+# Classes/Routes
+class home:
+    def GET(self):
+        return "home"
+
+
+if __name__ == "__main__":
+    app.run()
