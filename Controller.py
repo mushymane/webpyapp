479a29b21bf723dc111c2415f773f7f8fe8a83cb
Luigi Lantin <mushymanee@gmail.com>
2021-01-28 23:15:52 -0800

    added functionality

--- Controller.py
+++ Controller.py
@@ -1,17 +1,67 @@
 import web
+from Models import RegisterModel, LoginModel
+
+web.config.debug = False
 
 urls = (
-    '/', 'home'
+    '/', 'Home',
+    '/register', 'Register',
+    '/login', 'Login',
+    '/logout', 'Logout',
+    '/postregistration', 'PostRegistration',
+    '/check-login', 'CheckLogin'
 )
 
-render = web.template.render("Views/Templates", base="MainLayout")
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={'user': 'none'})
+session_data = session._initializer
+
+
+render = web.template.render("Views/Templates", base="MainLayout", globals={'session': session_data, 'current_user': session_data["user"]})
 
 
 # Classes/Routes
-class home:
+class Home:
     def GET(self):
         return render.Home()
+
+
+class Register:
+    def GET(self):
+        return render.Register()
+
+
+class Login:
+    def GET(self):
+        return render.Login()
+
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+
+        reg_model = RegisterModel.RegisterModel()
+        reg_model.insert_user(data)
+        return data.username
+
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        is_correct = login.check_user(data)
+
+        if is_correct:
+            session_data["user"] = is_correct
+            return is_correct
+
+        return "error"
+
+
+class Logout:
+    def GET(self):
+        session.kill()
+        return "success"
 
 
 if __name__ == "__main__":
---
+++ Models/RegisterModel.py
@@ -0,0 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+import bcrypt
+
+class RegisterModel:
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client.socialnetwork
+        self.Users = self.db.users
+
+    def insert_user(self, data):
+        hashed = bcrypt.hashpw(data.password.encode(), bcrypt.gensalt())
+
+        id = self.Users.insert({"username": data.username, "name": data.name, "password": hashed, "email": data.email})
+        print("uid", id)
+
+        myuser = self.Users.find_one({"username": data.username})
+
+        if bcrypt.checkpw("avocado1".encode(), myuser["password"]):
+            print("match")
